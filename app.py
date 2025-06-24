4d423bfe1844eeec47056fb7d35e1a27abd4235f
Priyanshu Sengupta <141814919+prynshus@users.noreply.github.com>
2025-06-24 11:42:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,url_for,request
+from nltk.stem import WordNetLemmatizer
+import pickle
+import nltk
+from nltk.corpus import stopwords
+
+gb_clf = pickle.load(open('model.pkl', 'rb'))
+vectorizer =pickle.load(open('transform.pkl', 'rb'))
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        Lemmatizer = WordNetLemmatizer()
+        words = nltk.word_tokenize(message)
+        words = [Lemmatizer.lemmatize(word, pos='v') for word in words if word not in set(stopwords.words('english'))]
+        message = ' '.join(words)
+        vect = vectorizer.transform([message]).toarray()
+        my_prediction = gb_clf.predict(vect)
+    return render_template('results.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
